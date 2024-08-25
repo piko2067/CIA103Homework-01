139e3746cd2ce2acf50abb1cd404100eea0e469b
Piko <piko2067@gmail.com>
2024-08-26 00:32:05 +0800

    first commit

---
+++ src/hw2/Test4Numbers.java
@@ -0,0 +1,11 @@
+package hw2;
+
+public class Test4Numbers {
+	public static void main(String[] args) {
+		int i;
+		for (i = 0; i <= 100; i += 4) {
+			System.out.println(i);
+		}
+		System.out.println();
+	}
+}
---
+++ src/hw2/TestNineNine.java
@@ -0,0 +1,38 @@
+package hw2;
+
+public class TestNineNine {
+	public static void main(String[] args) {
+//		使用for迴圈 + while迴圈
+		int i, j;
+		for (i = 1; i <= 9; i++) {
+			j = 1;
+			while (j++ <= 9)
+				System.out.print(i + "*" + j + "=" + i * j + "\t");
+			System.out.println();
+		}
+
+//		使用for迴圈 + do while迴圈
+		int i, j;
+		for (i = 1; i <= 9; i++) {
+			j = 1;
+			do {
+				System.out.print(i + "*" + j + "=" + i * j + "\t");
+				j++;
+			} while (j <= 9);
+			System.out.println();
+		}
+
+//		使用while迴圈 + do while迴圈	
+		int i, j;
+		i = 1;
+		while (i <= 9) {
+			j = 1;
+			do {
+				System.out.print(i + "*" + j + "=" + i * j + "\t");
+				j++;
+			} while (j <= 9);
+			i++;
+			System.out.println();
+		}
+	}
+}

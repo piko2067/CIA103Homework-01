cf5c1534f49e3d7305e754922e0ef34f9240122f
Piko <piko2067@gmail.com>
2024-09-09 22:43:39 +0800

    commit Hw03 2

--- src/hw3/Hw02.java
+++ src/hw3/Hw02.java
@@ -1,20 +1,45 @@
 package hw3;
+
 import java.util.Scanner;
+
 public class Hw02 {
 	public static void main(String[] args) {
 //		請設計一隻程式,會亂數產生一個0~9的數字,然後可以玩猜數字遊戲,猜錯會顯示錯誤訊息,猜對則顯示正確訊息,如圖示結果:
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("開始猜數字吧!");
+//		int seatNo = (int) (Math.random() * 10);
+//		int a;
+//		while (true) {
+//			if (sc.hasNextInt()) {
+//				a = sc.nextInt();
+//				if (a == seatNo) {
+//					System.out.println("答對了答案就是" + seatNo);
+//					break;
+//				} else {
+//					System.out.println("猜錯了");
+//				}
+//			}
+//		}
+//		進階功能:產生0~100亂數,每次猜就會提示你是大於還是小於正確答案
 		Scanner sc = new Scanner(System.in);
 		System.out.println("開始猜數字吧!");
-		int seatNo = (int) (Math.random() * 10);
+		int seatNo = (int) (Math.random() * 101);
 		int a;
 		while (true) {
+			System.out.println("請輸入1到100之間的數字：");
 			if (sc.hasNextInt()) {
 				a = sc.nextInt();
-				if (a == seatNo) {
-					System.out.println("答對了答案就是" + seatNo);
-					break;
+				if (a >= 0 && a <= 100) {
+					if (a == seatNo) {
+						System.out.println("答對了答案就是" + seatNo);
+						break;
+					} else if (a > seatNo) {
+						System.out.println("猜錯了!數字要更小");
+					} else {
+						System.out.println("猜錯了!數字要更大");
+					}
 				} else {
-					System.out.println("猜錯了");
+					System.out.println("錯誤! 請輸入1到100之間的數字!");
 				}
 			}
 		}
--- src/hw3/Hw03.java
+++ src/hw3/Hw03.java
@@ -1,5 +1,8 @@
 package hw3;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class Hw03 {
@@ -29,5 +32,30 @@ public class Hw03 {
 				}
 			}
 		}
+//		進階挑戰:輸入不要的數字後,直接亂數印出6個號碼且不得重複
+//		Scanner sc = new Scanner(System.in);
+//		int a;
+//		while (true) {
+//			System.out.print("阿文...請輸入你討厭哪個數字? ");
+//			if (sc.hasNextInt()) {
+//				a = sc.nextInt();
+//				if (a >= 1 && a < 10) {
+//					List<Integer> validNumbers = new ArrayList<>();
+//					for (int x = 1; x <= 49; x++) {
+//						if (x / 10 != a && x % 10 != a) {
+//							validNumbers.add(x);
+//						}
+//					}
+//					Collections.shuffle(validNumbers);
+//					List<Integer> selectedNumbers = validNumbers.subList(0, 6);
+//					System.out.println("隨機選出的6個號碼是: " + selectedNumbers);
+//					break;
+//				} else {
+//					System.out.println("錯誤! 請輸入1到9之間的數字!");
+//				}
+//			} else {
+//				System.out.println("錯誤! 請輸入一個整數!");
+//			}
+//		}
 	}
 }
